1e14939b3bebfdfad29b2be1fead2976983f9a57
iribarrenfederico <iribarrenfederico@gmail.com>
2021-04-17 08:08:00 -0300

    segunda clase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hola, soy Federico, esta va a ser mi tienda.
+          Hola, soy Federico, esta va a ser mi tienda!.
         </p>
         <a
           className="App-link"
