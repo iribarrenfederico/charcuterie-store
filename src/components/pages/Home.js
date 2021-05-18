e0adebce14d4f9db9aec050473129bafe64d435e
iribarrenfederico <iribarrenfederico@gmail.com>
2021-05-18 06:09:45 -0300

    fix files

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import Counter from "./Counter";
+import Counter from "../Counter";
 import { useParams } from "react-router-dom";
 
 const Home = () => {
