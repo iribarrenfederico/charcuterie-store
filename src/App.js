603abac92ec1c6d008832d9fc492c5b12f4fb5f4
iribarrenfederico <iribarrenfederico@gmail.com>
2021-05-18 06:09:08 -0300

    arreglo archivos en carpetas

--- src/App.js
+++ src/App.js
@@ -1,22 +1,105 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import "./Css/ItemListContainer.css";
-import { NavBar } from "./components/NavBar";
-import Cart from "./components/cart/Cart";
-import Home from "./components/Home";
-import ItemDetailContainer from "./components/ItemDetailContainer";
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom'
+import {Carrito} from './context/cartContext'
+
+import Cabecera from './components/header/Header'
+import {enlacesNav} from './components/values/values'
+
+import Home from './components/pages/home'
+
+import ItemListContainer from './components/itemlistcontainer/ItemListContainer.js'
+import ItemDetailContainer from './components/itemlistcontainer/ItemDetailContainer'
+import ItemDetail from './components/itemlistcontainer/ItemDetail.js'
+
+import CartIconContainer from './components/cart/cartIconContainer'
+
+
+import {ListaProductos} from './components/values/values'
+
+import {TablaPrecios} from './components/pricelist/pricelist'
+
+import Footer from './components/footer/footer'
 
 export default function App() {
-  const category = ["category1", "category2"];
-  return (
-    <>
-      <NavBar categories={category} />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/cart" exact component={Cart} />
-        <Route path="/item/:title" exact component={ItemDetailContainer} />
-      </Switch>
-    </>
+
+return (
+<Carrito>     
+
+  <Router basename={process.env.PUBLIC_URL}>
+    <div className="App">
+
+      <Cabecera
+                titulo = 'La cocina de la Pipi' 
+                logo = {process.env.PUBLIC_URL + '/images/logo-transparente.png'}
+                enlaces={enlacesNav}
+      />
+     
+
+    <CartIconContainer/>
+  
+    
+    <main>
+        <Switch>
+          
+          <Route path="/home">
+            <Home/>
+          </Route>
+          
+          <Route path="/recetas">
+            <h2>Recetas</h2>
+          </Route>
+              
+          <Route path="/precios">
+            <TablaPrecios listado={ListaProductos} />
+          </Route>
+   
+          <Route exact path="/productos/:familia/:id">
+              <ItemDetailContainer listado={ListaProductos} />
+          </Route>
+
+          <Route exact path="/productos/:familia">
+              <ItemListContainer listado={ListaProductos} />
+          </Route>
+
+          <Route exact path="/productos">
+              <ItemListContainer listado={ListaProductos} />
+          </Route>
+          
+          <Route path="/detalle">
+              <ItemDetail listado={ListaProductos} />
+          </Route>
+
+          <Route path="/pedidos">
+              <CartIconContainer/>
+          </Route>
+     
+          <Route path="/">
+            <OnLoadPage/>
+          </Route>
+         
+        </Switch>
+      </main>
+
+      <Footer
+        titulo = 'La cocina de la Pipi'
+        logo = {process.env.PUBLIC_URL + '/images/logo-transparente.png'}
+        enlaces={enlacesNav}
+        telefono="11 15 41234-1234"
+        correo="pedidos@correo.com"
+      />
+
+    </div>
+   </Router>
+
+</Carrito> 
   );
 }
 
+function OnLoadPage() {
+  const history = useHistory();
+    history.push('/home');
+    return(
+        <>
+        </>
+      )
+  }
