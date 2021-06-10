0449082d4623d440d9d0b959a53abcd5c59b6c6a
iribarrenfederico <iribarrenfederico@gmail.com>
2021-06-10 18:08:48 -0300

    modal delete

--- src/components/cart/cartIcon.js
+++ src/components/cart/cartIcon.js
@@ -1,5 +1,4 @@
 import React, {useState,useEffect} from 'react'
-import Modal from '../modal/modal'
 import {Link} from "react-router-dom";
 import {Loading,randomKey} from '../../helpers/helpers'
 
