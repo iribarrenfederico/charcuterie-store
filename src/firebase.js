29bdabf3e8fcad078c4d6c17f173eebfb77317ba
iribarrenfederico <iribarrenfederico@gmail.com>
2021-06-10 20:19:59 -0300

    fb

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const configFirebase = {
-  appId: process.env.REACT_APP_APP_ID,
-  apiKey: process.env.REACT_APP_API_KEY,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-}
-
+  apiKey: "AIzaSyB0DNS_euYJaM10akcbmF3wfE_Sfiazq3s",
+  authDomain: "templefoods-5b990.firebaseapp.com",
+  projectId: "templefoods-5b990",
+  storageBucket: "templefoods-5b990.appspot.com",
+  messagingSenderId: "568526704107",
+  appId: "1:568526704107:web:afc0a01f39b6d1bf241dae",
+  measurementId: "G-ZME2F1M0CD"
+};
 firebase.initializeApp(configFirebase)
 
 const auth = firebase.auth(); 
