5555c4288b8585b0aaedd118a11fbfa6594227ff
iribarrenfederico <iribarrenfederico@gmail.com>
2021-06-10 17:51:17 -0300

    comitteando ultimo momento

--- src/components/itemlistcontainer/ItemListContainer.js
+++ src/components/itemlistcontainer/ItemListContainer.js
@@ -1,72 +1,42 @@
-import React, { useState, useEffect,useContext } from 'react'
+import React, { useState, useEffect } from 'react'
 import {useParams} from 'react-router-dom'
 
 import Productos from './ItemList'
+import {useCartContext} from '../../context/cartContext'
 import {Loading} from '../../helpers/helpers'
-import  {useCartContext} from '../../context/cartContext'
-import db from '../../firebase/index'
+import {fire} from '../../firebase'
 
-import './style.css'
-
-export default function ItemListContainer({listado}) {
+export default function ItemListContainer() {
  
-    const [ListadoProductos, SetListadoProductos] = useState([])
     const {familia} = useParams()
+    const [cart, cartTask] = useCartContext()
+    const [lstProductos, SetLstProductos] = useState([])
 
  useEffect(() => {
-         db.collection("items").get().then( (querySnapshot) => {
-            let salida = []
-            querySnapshot.forEach((doc) => {
-              salida.push({...doc.data(), id: doc.id})
-          })
-              SetListadoProductos(salida)
-          }
-      )
- },[familia]);
-
-
- useEffect(() => {
-  familia && (
-         db.collection("items").where("familia","==",familia).get()
-         .then( (querySnapshot) => {
-            let salida = []
-            querySnapshot.forEach((doc) => {
-              salida.push({...doc.data(), id: doc.id})
-          })
-              SetListadoProductos(salida)
-          }
-      )
-)
-
-
- },[familia]);
+    fire.getCollection(SetLstProductos,"items",familia?{where:["familia","==",familia]}:{})
+ },[familia,cart]);
 
  return(
-    
     <div className="row justify-content-center py-3 mw-100">  
         <div className="col-12 pb-4">
-            <h1>Our Philosophy is a simple one.</h1>
+            <h1>No nos guardamos ning&uacute;n secreto, lo hacemos con amor.</h1>
         </div>
         <div className="col-10">
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-12">
                         <section className="pt-1 mt-3 mx-3">
-                            <p className="pb-2">At Tempus we only use ex-breeding sows and ex-dairy beef in our charcuterie.</p>
-                            <p>This ethos is also part of our commitment to utilising the food-chain correctly.</p>
+                            <p className="pb-2">Todas nuestras milanesas est&aacute;n rebozadas con rebozador Preferido y Avena natural, 
+                                                condimentadas con ajo y perejil, no tienen sal y tienen mucho amor.</p>
+                            <p>Todas nuestras hamburguesas son preparadas y congeladas en el d&iacute;a para asegurar su calidad.</p>
                         </section>
                     </div>
                 </div>
 
-                {
-                  ListadoProductos.length > 0 ? 
-                  <Productos listaProductos = {ListadoProductos}/>
-                    :
-                  <Loading size="8" space="5"/>
-                }
+                {lstProductos.length > 0 ? <Productos listaProductos = {lstProductos}/> : <Loading size="8" space="5"/>}
+
              </div>
         </div>             
     </div>
-
  )  
 }
